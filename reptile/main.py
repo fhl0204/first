6b850bd85480ae98d493c2f1e4f8f982bd9eb054
fhl <1390671039@qq.com>
2017-11-20 22:15:37 +0800

    all of basic

--- reptile/main.py
+++ reptile/main.py
@@ -5,8 +5,12 @@ import urllib2
 import re
 import csv
 import codecs
+from tmp import get_all_list
 
 codes = ['513050', '001668', '110011', '110012', '000011', '210009', '000961']
+url = 'http://fund.eastmoney.com/allfund.html'
+
+codes = get_all_list(url)
 
 filename = 'result.csv'
 
@@ -36,19 +40,31 @@ def parse_one(code):
     content = m.group(0)  # dateIemm02
 
     pattern2 = '<dd class="dataNums">.*?</dd>'
-    content2 = re.search(pattern2, content).group(0)
+    content2 = re.search(pattern2, content)
+    if content2:
+        content2 = content2.group(0)
+    else:
+        content2 = 'NULL'
 
     pattern3 = '<span\sclass="[^">]+">([^<>]+)</span>'
     content3 = re.search(pattern3, content2)
-    price = content3.group(1)
+    if content3:
+        price = content3.group(1)
+    else:
+        price = 'NULL'
 
     pattern4 = '<dt>.*?</dt>'  # 带时间的dt->p->
     content4 = re.search(pattern4, content).group(0)
 
     pattern5 = '\(</span>.*?<'  # (</span>2016-10-27)<
-    content5 = re.search(pattern5, content4).group(0)
+    content5 = re.search(pattern5, content4)
+    if content5:
+        content5 = content5.group()
 
-    time = content5[8: len(content5) - 2]
+    if content5:
+        time = content5[8: len(content5) - 2]
+    else:
+        time = 'NULL'
 
     pattern6 = '<div class="fundDetail-tit">.*?</div>'
     content6 = re.search(pattern6, html).group(0)
@@ -56,25 +72,50 @@ def parse_one(code):
     pattern7 = 'left">.*?<span'
 
     content7 = re.search(pattern7, content6).group(0)
+
     name = content7[6:len(content7) - 5]
 
     pattern8 = '<dd><span>近1月：</span><span class="ui-font-middle (ui-color-red|ui-color-green)? ui-num">(-?\d{1,3}.\d{2}%|--)</span></dd>'
-    month1 = re.search(pattern8, html).group(2)
+    month1 = re.search(pattern8, html)
+    if month1:
+        month1 = month1.group(2)
+    else:
+        month1 = 'NULL'
 
     pattern9 = '<dd><span>近3月：</span><span class="ui-font-middle (ui-color-red|ui-color-green)? ui-num">(-?\d{1,3}.\d{2}%|--)</span></dd>'
-    month3 = re.search(pattern9, html).group(2)
+    month3 = re.search(pattern9, html)
+    if month3:
+        month3 = month3.group(2)
+    else:
+        month3 = 'NULL'
 
     pattern10 = '<dd><span>近6月：</span><span class="ui-font-middle (ui-color-red|ui-color-green)? ui-num">(-?\d{1,5}.\d{2}%|--)</span></dd>'
-    month6 = re.search(pattern10, html).group(2)
+    month6 = re.search(pattern10, html)
+    if month6:
+        month6 = month6.group(2)
+    else:
+        month6 = 'NULL'
 
     pattern11 = '<dd><span>近1年：</span><span class="ui-font-middle (ui-color-red|ui-color-green)? ui-num">(-?\d{1,3}.\d{2}%|--)</span></dd></dl>'
-    year1 = re.search(pattern11, html).group(2)
+    year1 = re.search(pattern11, html)
+    if year1:
+        year1 = year1.group(2)
+    else:
+        year1 = 'NULL'
 
     pattern12 = '<dd><span>近3年：</span><span class="ui-font-middle (ui-color-red|ui-color-green)? ui-num">(-?\d{1,3}.\d{2}%|--)</span></dd>'
-    year3 = re.search(pattern12, html).group(2)
+    year3 = re.search(pattern12, html)
+    if year3:
+        year3 = year3.group(2)
+    else:
+        year3 = 'NULL'
 
     pattern13 = '<dd><span>成立来：</span><span class="ui-font-middle (ui-color-red|ui-color-green)? ui-num">(-?\d{1,5}.\d{2}%|--)</span></dd>'
-    until_now = re.search(pattern13, html).group(2)
+    until_now = re.search(pattern13, html)
+    if until_now:
+        until_now = until_now.group(2)
+    else:
+        until_now = 'NULL'
 
     writeResult(name, code, price, time, month1, month3, month6, year1, year3, until_now)
 
--- reptile/tmp.py
+++ reptile/tmp.py
@@ -1,31 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import urllib2
+
 import re
 import requests
 from bs4 import BeautifulSoup
-from main import getHtml
 
 url = 'http://fund.eastmoney.com/allfund.html'
-url = 'http://fund.eastmoney.com/000011.html'
-html = requests.get(url).content
+#url = 'http://fund.eastmoney.com/000011.html'
 
-#html = getHtml(url)
 
-soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
 
-# tags = soup.find_all('li', class_='increaseAmount')
-# tags = tags.find_all('div')
+def get_all_list(url):
+    html = requests.get(url).content
 
-tags=soup.find_all(class_='ui-font-middle ui-color-red ui-num')
-# m1=tags[3].string
-# y1=tags[4].string
-# m3=tags[5].string
-# y3=tags[6].string
-# m6=tags[7].string
-# rece=tags[8].string
-# detail={'近1月':m1,'近3月':m3,'近6月':m6,'近1年':y1,'近3年':y3,'成立来':rece}
-# print(detail)
 
-for tag in tags:
-    print tag
+    soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
+
+    # tags = soup.find_all('li', class_='increaseAmount')
+    # tags = tags.find_all('div')
+
+    tags=soup.find_all('li', class_='b')
+
+    # m1=tags[3].string
+    # y1=tags[4].string
+    # m3=tags[5].string
+    # y3=tags[6].string
+    # m6=tags[7].string
+    # rece=tags[8].string
+    # detail={'近1月':m1,'近3月':m3,'近6月':m6,'近1年':y1,'近3年':y3,'成立来':rece}
+    # print(detail)
+
+    all_list = []
+    for tag in tags:
+        pattern = '\d{6}'
+        tag = tag.prettify()
+        result = re.search(pattern, tag)
+        if result:
+            all_list.append(result.group())
+
+    print all_list
+    return all_list
